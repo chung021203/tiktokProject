ac89e3201abfbeddbb9a3a6dc2eb918c4fde7301
chung021203 <140178325+chung021203@users.noreply.github.com>
2024-01-30 15:52:21 +0700

    Xây dựng cơ chế router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Following from './pages/Following';
 
 function App() {
-    return <h1>xin chao</h1>;
+    return (
+        <Router>
+            <div className="App"></div>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/following" element={<Following />}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Following/index.js
@@ -0,0 +1,5 @@
+function Following() {
+    return <div className="container">Following page</div>;
+}
+
+export default Following;
